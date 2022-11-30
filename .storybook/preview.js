81a67cff596df00182d89de2e343cbdf3bf0b0c6
Robert Vogt <robert@smartive.ch>
2022-11-30 16:53:18 +0100

    docs: Sort Stories Alphabetically

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,18 @@
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      method: "alphabetical",
+      order: [],
+      includeName: true,
     },
   },
 };
