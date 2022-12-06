f47d26521bf0149d36c450181f67daabb88e78dc
Robert Vogt <robert@smartive.ch>
2022-12-06 20:06:54 +0100

    fix(Button): Add Background Position for Animation

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -50,7 +50,7 @@ const colorMap: Record<BaseButtonProps["color"], string> = {
   Violet:
     "bg-violet-600 hover:bg-violet-700 active:bg-violet-700 outline hover:outline-3 hover:outline-violet-100 active:outline-4 active:outline-violet-200",
   Gradient:
-    "bg-gradient-50-50 hover:bg-gradient-30-70 active:bg-gradient-20-80 from-pink-500 to-violet-500 outline hover:outline-3 hover:outline-violet-100 active:outline-4 active:outline-violet-200",
+    "bg-200% bg-left bg-gradient-50-50 hover:bg-center active:bg-right from-pink-500 to-violet-500 outline hover:outline-3 hover:outline-violet-100 active:outline-4 active:outline-violet-200",
 };
 
 const sizeMap: Record<BaseButtonProps["size"], string> = {
@@ -71,6 +71,7 @@ const isButton = (props: ButtonProps): props is HTMLButtonProps =>
 
 export const Button: FC<ButtonProps> = (props) => {
   const className = merge([
+    "will-change-auto",
     "text-white",
     "rounded-lg",
     "inline-flex flex-row items-center",
